786fa6d232d9679182ffb87653c7e57c58de4d51
KingSlme <djgreene100@gmail.com>
2024-01-01 20:38:32 -0500

    Add initial src

---
+++ src/argument_handler.cpp
@@ -0,0 +1,10 @@
+#include "../include/argument_handler.h"
+
+#include "../include/file_reader.h"
+
+int handleArguments(int argc, char* argv[]) {
+    if (argc < 3) { // argc def 1, so we need at least 3
+
+
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "../include/argument_handler.h"
+
+int main(int argc, char* argv[]) {
+    return handleArguments(argc, argv);
+}
