509cce71a24a274fb40520aa78df4f748bfca23f
KingSlme <djgreene100@gmail.com>
2024-01-02 21:51:59 -0500

    Add build 1 src

--- src/argument_handler.cpp
+++ src/argument_handler.cpp
@@ -1,10 +1,30 @@
 #include "../include/argument_handler.h"
 
 #include "../include/file_reader.h"
+#include <iostream>
+
+#define RESET         "\033[0m"
+#define BRIGHT_RED    "\033[91m"
+#define BRIGHT_YELLOW "\033[93m"
 
 int handleArguments(int argc, char* argv[]) {
-    if (argc < 3) { // argc def 1, so we need at least 3
-
-
+    if (argc == 4 && (std::string(argv[3]) == "words" || std::string(argv[3]) == "lines")) {
+        std::vector<std::string> lines = getFileLines(argv[1]);
+        if (lines.size() == 0)
+            return 1;
+        std::vector<std::pair<std::string, std::string>> matches = regexMatch(lines, argv[2]);
+        if (matches.size() == 0)
+            return 1;
+        std::cout << BRIGHT_YELLOW << "\nMatches: " << RESET << std::endl;
+        for (const std::pair<std::string, std::string> &match : matches)
+            if (std::string(argv[3]) == "words")
+                std::cout << match.first << std::endl;
+            else
+                std::cout << match.second << std::endl;
+        return 0;
+    } else {
+        std::cerr << BRIGHT_RED << "Usage: <file path> <regex pattern> <words|lines>" << RESET << std::endl;
+        return 1;
     }
 }
+
--- src/file_reader.cpp
+++ src/file_reader.cpp
@@ -1,1 +1,40 @@
 #include "../include/file_reader.h"
+
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+#define RESET         "\033[0m"
+#define BRIGHT_RED    "\033[91m"
+#define BRIGHT_YELLOW "\033[93m"
+
+std::vector<std::string> getFileLines(const std::string &filePath) {
+    std::ifstream file(filePath);
+    std::vector<std::string> lines;
+    if (!file.is_open()) {
+        std::cerr << BRIGHT_RED << "Cannot find file '" << filePath << "'" << RESET << std::endl; 
+        return lines;
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        lines.push_back(line);
+    }
+    file.close();
+    if (lines.size() == 0)
+        std::cerr << BRIGHT_RED << "File is empty" << RESET << std::endl;
+    return lines;
+}
+
+std::vector<std::pair<std::string, std::string>> regexMatch(const std::vector<std::string> &lines, std::string regexPattern) {
+    std::regex pattern(regexPattern);
+    std::vector<std::pair<std::string, std::string>> matches;
+    std::smatch match;
+    for (const std::string &line : lines) {
+        if (std::regex_search(line, match, pattern)) {
+            matches.push_back(std::make_pair(match.str(), line));
+        }
+    }
+    if (matches.size() == 0)
+        std::cerr << BRIGHT_RED << "No matches" << RESET << std::endl;;
+    return matches;
+}
