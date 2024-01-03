da46158e8c5d98bd0b1cfb32d2f50a6823d753cc
KingSlme <djgreene100@gmail.com>
2024-01-02 21:52:31 -0500

    Add build 1 headers

--- include/file_reader.h
+++ include/file_reader.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include <vector>
+#include <utility>
+#include <string>
+
+std::vector<std::string> getFileLines(const std::string &filePath);
+
+std::vector<std::pair<std::string, std::string>> regexMatch(const std::vector<std::string> &lines, std::string regexPattern);
